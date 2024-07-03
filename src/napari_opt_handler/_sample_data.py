100a010bbe28c4bf998762d6924b8cd6550ba80d
palec87 <david@stanka.de>
2024-07-03 18:04:04 +0100

    setting up the plugin deploy

--- src/napari_opt_handler/_sample_data.py
+++ src/napari_opt_handler/_sample_data.py
@@ -6,16 +6,55 @@ see: https://napari.org/stable/plugins/guides.html?#sample-data
 
 Replace code below according to your needs.
 """
+
 from __future__ import annotations
 
-import numpy
+import numpy as np
+import pooch
+from skimage.io import imread
+
+base_url = (
+    "https://raw.githubusercontent.com/QBioImaging/napari-opt-handler/"
+    "main/src/napari_opt_handler/sample_data"
+)
 
 
 def make_sample_data():
     """Generates an image"""
-    # Return list of tuples
-    # [(data1, add_image_kwargs1), (data2, add_image_kwargs2)]
-    # Check the documentation for more information about the
-    # add_image_kwargs
-    # https://napari.org/stable/api/napari.Viewer.html#napari.Viewer.add_image
-    return [(numpy.random.rand(512, 512), {})]
+    # load image from sample data
+    data = []
+    dlist = [
+        "16-48-16_0000.tiff",
+        "16-48-17_0001.tiff",
+        "16-48-18_0002.tiff",
+        "16-48-18_0003.tiff",
+        "16-48-19_0004.tiff",
+        "16-48-20_0005.tiff",
+        "16-48-21_0006.tiff",
+    ]
+
+    for filename in dlist:
+        url = f"{base_url}/{filename}"
+        file = pooch.retrieve(url=url, known_hash=None)
+        data.append(imread(file))
+    data = np.stack(data, axis=0)
+    data_hot = imread(
+        pooch.retrieve(url=f"{base_url}/corr_hot.tiff", known_hash=None),
+    )
+
+    data_dark = imread(
+        pooch.retrieve(url=f"{base_url}/dark_field.tiff", known_hash=None),
+    )
+
+    data_bright = imread(
+        pooch.retrieve(url=f"{base_url}/flat_field.tiff", known_hash=None),
+    )
+
+    # TODO no idea how to set layers to optWidget.image_layer_select.value
+    # Did I missed something in napari.current_viewer()
+    return [
+        (data_hot, {"name": "bad"}, "Image"),
+        (data_dark, {"name": "dark"}, "Image"),
+        (data_bright, {"name": "bright"}, "Image"),
+        (data, {"name": "OPT data"}, "Image"),
+    ]
