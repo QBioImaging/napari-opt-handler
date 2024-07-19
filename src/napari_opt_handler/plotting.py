2ba4a0f9252c5cd46fbf1d5c7e61f4df99e505de
palec87 <david@stanka.de>
2024-07-19 15:47:17 +0100

    plot bleaching correction

--- src/napari_opt_handler/plotting.py
+++ src/napari_opt_handler/plotting.py
@@ -1,13 +1,14 @@
-import numpy as np
 import matplotlib as mpl
-from matplotlib.figure import Figure
+import numpy as np
 from matplotlib.backends.backend_qt5agg import (
-    FigureCanvasQTAgg as FigureCanvas
+    FigureCanvasQTAgg as FigureCanvas,
 )
-
+from matplotlib.figure import Figure
 from qtpy.QtWidgets import (
+    QDialog,
+    QSizePolicy,
     QVBoxLayout,
-    QWidget, QDialog, QSizePolicy,
+    QWidget,
 )
 
 
@@ -19,19 +20,28 @@ class PlotDialog(QDialog):
     on particular tasks during the overall experiment,
     as well as per OPT step.
     """
-    def __init__(self, parent, report: dict) -> None:
-        super(PlotDialog, self).__init__(parent)
+
+    def __init__(self, parent, report: dict, canvas: str) -> None:
+        # super(PlotDialog, self).__init__(parent)
+        super().__init__(parent)
         self.mainWidget = QWidget()
+        self.canvas = canvas
         layout = QVBoxLayout(self.mainWidget)
-        self.canvas = IntCorrCanvas(report, self.mainWidget,
-                                    width=300, height=300)
+        if self.canvas == "IntCorrCanvas":
+            self.canvas = IntCorrCanvas(
+                report, self.mainWidget, width=300, height=300
+            )
+        elif self.canvas == "FlBleachCanvas":
+            self.canvas = FlBleachCanvas(
+                report, self.mainWidget, width=300, height=300
+            )
         layout.addWidget(self.canvas)
         self.setLayout(layout)
 
 
 class IntCorrCanvas(FigureCanvas):
     def __init__(self, data_dict, parent=None, width=300, height=300):
-        """ Plot of the report
+        """Plot of the report
 
         Args:
             corr_dict (dict): report data dictionary
@@ -49,9 +59,9 @@ class IntCorrCanvas(FigureCanvas):
         FigureCanvas.__init__(self, fig)
         self.setParent(parent)
 
-        FigureCanvas.setSizePolicy(self,
-                                   QSizePolicy.Expanding,
-                                   QSizePolicy.Expanding)
+        FigureCanvas.setSizePolicy(
+            self, QSizePolicy.Expanding, QSizePolicy.Expanding
+        )
         FigureCanvas.updateGeometry(self)
 
     def createFigure(self, data_dict: dict) -> None:
@@ -62,14 +72,55 @@ class IntCorrCanvas(FigureCanvas):
         """
         my_cmap = mpl.colormaps.get_cmap("viridis")
         colors = my_cmap(np.linspace(0, 1, 2))
-        self.ax1.plot(data_dict['stack_orig_int'],
-                      label='Original',
-                      color=colors[0])
+        self.ax1.plot(
+            data_dict["stack_orig_int"], label="Original", color=colors[0]
+        )
 
-        self.ax1.plot(data_dict['stack_corr_int'],
-                      label='Corrected',
-                      color=colors[1])
+        self.ax1.plot(
+            data_dict["stack_corr_int"], label="Corrected", color=colors[1]
+        )
 
-        self.ax1.set_xlabel('OPT step number')
-        self.ax1.set_ylabel('Intensity')
+        self.ax1.set_xlabel("OPT step number")
+        self.ax1.set_ylabel("Intensity")
         self.ax1.legend()
+
+
+class FlBleachCanvas(FigureCanvas):
+    def __init__(self, data_dict, parent=None, width=300, height=300):
+        """Plot of the report
+
+        Args:
+            corr_dict (dict): report data dictionary
+            parent (_type_, optional): parent class. Defaults to None.
+            width (int, optional): width of the plot in pixels.
+                Defaults to 300.
+            height (int, optional): height of the plot in pixels.
+                Defaults to 300.
+        """
+        fig = Figure(figsize=(width, height))
+        self.ax1 = fig.add_subplot()
+
+        self.createFigure(data_dict)
+
+        FigureCanvas.__init__(self, fig)
+        self.setParent(parent)
+
+        FigureCanvas.setSizePolicy(
+            self, QSizePolicy.Expanding, QSizePolicy.Expanding
+        )
+        FigureCanvas.updateGeometry(self)
+
+    def createFigure(self, data_dict: dict) -> None:
+        """Create report plot.
+
+        Args:
+            data_dict (dict): Intensity correction dictionary.
+        """
+        my_cmap = mpl.colormaps.get_cmap("Greys")
+        self.ax1.imshow(
+            data_dict["corr_factors"],
+            cmap=my_cmap,
+            aspect="auto",
+        )
+        self.ax1.set_xlabel("Camera row index")
+        self.ax1.set_ylabel("Angle step index")
