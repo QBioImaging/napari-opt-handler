fixing tests for fl bleaching, removing normalization

src/napari_opt_handler/_tests/test_corr.py
@@ -55,16 +55,16 @@ return_stack1 = np.ones((5, 5, 5)) * 10
 stack2 = np.ones((5, 5, 5)) * 5
 stack2[0] = np.ones((5, 5)) * 10
 
-fl_bleach_corr2 = np.ones((5, 5)) * 5
-fl_bleach_corr2[0] = 10
+fl_bleach_corr2 = np.ones((5, 5)) * 5 / 10
+fl_bleach_corr2[0] = 10 / 10
 
 stack3 = np.ones((6, 4, 3)) * 5
 stack3[0] = np.ones((4, 3)) * 10
 stack3[1] = np.ones((4, 3)) * 7
 
-fl_bleach_corr3 = np.ones((6, 4)) * 5
-fl_bleach_corr3[0] = 10
-fl_bleach_corr3[1] = 7
+fl_bleach_corr3 = np.ones((6, 4)) * 5 / 10
+fl_bleach_corr3[0] = 10 / 10
+fl_bleach_corr3[1] = 7 / 10
 
 
 @pytest.mark.parametrize(
@@ -447,14 +447,13 @@ def test_correct_int_except2():
         (
             (hot2, 4, dark1, bright2),
             stack1,
-            (np.ones(stack1.shape), {"corr_factors": stack1[0]}),
+            (stack1, {"corr_factors": np.ones(stack1[0].shape)}),
         ),
-        # stack2 has values 10, will only lead to normalization
         (
             (),
             stack2,
             (
-                np.ones(stack2.shape),
+                np.ones(stack2.shape) * 10,
                 {"corr_factors": fl_bleach_corr2},
             ),
         ),
@@ -462,7 +461,7 @@ def test_correct_int_except2():
             (),
             stack3,
             (
-                np.ones(stack3.shape),
+                np.ones(stack3.shape) * 10,
                 {"corr_factors": fl_bleach_corr3},
             ),
         ),

src/napari_opt_handler/_tests/test_widget.py
@@ -186,7 +186,7 @@ def test_corrIntensity1(request):
 # test correctFlBleach method
 def test_correctFlBleach1(request, monkeypatch):
     viewer, widget = request.getfixturevalue("prepare_widget_data1")
-    expected = np.ones((10, 5, 5)).astype(np.float64)
+    expected = (10 * np.ones((10, 5, 5))).astype(np.uint16)
 
     widget.inplace.val, widget.track.val = False, False
     widget.updateHistoryFlags()
@@ -213,7 +213,7 @@ def test_correctFlBleach1(request, monkeypatch):
 
 def test_correctFlBleach2(request, monkeypatch):
     viewer, widget = request.getfixturevalue("prepare_widget_data1")
-    expected = np.ones((10, 5, 5)).astype(np.float64)
+    expected = np.ones((10, 5, 5)).astype(np.uint16)
 
     widget.inplace.val, widget.track.val = False, False
     widget.updateHistoryFlags()

src/napari_opt_handler/_tests/test_widget_qtbot.py
@@ -5,6 +5,8 @@ Tests for Bad pixel correction because it contains
 a dialog window
 """
 
+from copy import copy
+
 import numpy as np
 import pytest
 from qtpy.QtWidgets import QMessageBox
@@ -14,7 +16,7 @@ ans1 = np.zeros((5, 5))
 ans1[2, 1] = 1
 
 # dead pixel should be identified too
-ans2 = ans1.copy()
+ans2 = copy(ans1)
 ans2[1, 2] = 0.1
 
 
@@ -62,7 +64,7 @@ def test_ask_correct_bad_pixels(request, monkeypatch):
     img[0, 1, 2] = 1
     img[0, 2, 1] = 30
 
-    ret = img.copy()
+    ret = copy(img)
     ret[0, 2, 1] = 10
     viewer.layers["img"].data = img
     widget.std_cutoff.val = 2.0
@@ -86,7 +88,7 @@ def test_ask_correctTrBleach(request, monkeypatch):
     )
     img = np.ones((5, 5, 5)) * 4
     img[0] = 5
-    ret = np.ones((5, 5, 5))
+    ret = np.ones((5, 5, 5)) * 5
 
     viewer.layers["img"].data = img
 